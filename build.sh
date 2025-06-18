915f52751889c2b87f5f6cd0a82cb10c28c6bc42
joshua-carlos-carr <admin@buzzdrones.com.au>
2025-06-18 21:03:59 +0930

    Add Node.js configuration and update Playwright installation

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -o errexit
 # Install Python dependencies
 pip install -r requirements.txt
 
-# Install Playwright browsers without requiring root
-export PLAYWRIGHT_BROWSERS_PATH=0
-playwright install --with-deps chromium 
+# Install Playwright browsers in user space
+export PLAYWRIGHT_BROWSERS_PATH=$HOME/.cache/ms-playwright
+python -m playwright install chromium --with-deps 
