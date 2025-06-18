12b7bbf1d1341f59bb63faa4e341b6ae05c85521
joshua-carlos-carr <admin@buzzdrones.com.au>
2025-06-18 21:17:27 +0930

    Fix pyppeteer installation in build process

--- build.sh
+++ build.sh
@@ -2,10 +2,14 @@
 # exit on error
 set -o errexit
 
+# Upgrade pip
+pip install --upgrade pip
+
 # Install Python dependencies
 pip install -r requirements.txt
 
-# Install pyppeteer browser
+# Install pyppeteer and its dependencies
+pip install pyppeteer
 python -c "import pyppeteer.chromium_downloader; pyppeteer.chromium_downloader.download_chromium()"
 
 # Test the health endpoint
